e24a414a75091635208c7f454006681c28daa716
glee0006-svg <glee0006@gmail.com>
2026-07-22 05:11:23 +0800

    Restore Soy Duck Leg image

--- components/Shop.jsx
+++ components/Shop.jsx
@@ -29,7 +29,7 @@ const PROD_DETAIL = {
 const PRODUCT_IMAGES = {
   d08: "/Duck Gizzard.jpg",
   d03: "/Salt Duck Leg.jpg",
-  d04: "/Duck Gizzard.jpg",
+  d04: "/Soy Duck Leg.jpg",
 };
 
 export default function Shop() {
