664ab3cabfd088156996b98219aa0acced251513
glee0006-svg <glee0006@gmail.com>
2026-08-08 05:44:55 +0800

    Update ImageSlot.jsx

--- components/ImageSlot.jsx
+++ components/ImageSlot.jsx
@@ -49,7 +49,7 @@ const [current, setCurrent] = useState(0);
           style={{
   width: "100%",
   height: "100%",
-  objectFit: "cover",
+  objectFit: "contain",
   display: "block",
   userSelect: "none",
 }}
